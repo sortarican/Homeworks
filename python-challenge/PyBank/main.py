fb322aec2248bef462cf8514a6d5e7a32b436f56
vallecillo <61523833+sortarican@users.noreply.github.com>
2020-03-29 22:48:33 -0400

    hw push

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -4,15 +4,14 @@ import csv
 tot_profit = 0
 max_diff = 0
 min_diff = 0
-
+num_mo = 0
+dif_tot = 0
 
 #read budget data
-
 datapath = os.path.join("..", "Resources", "budget_data.csv")
 
 with open(datapath) as datafile:
-    
-    datareader = csv.reader(datafile, delimiter=",")
+    datareader = csv.reader(datafile)
 
     #ignore headers
     data_header = next(datareader)        
@@ -25,33 +24,38 @@ with open(datapath) as datafile:
         #net profit/loss for period
         tot_profit += profit
 
-        #to begin calculations on 2nd row of data
-        first_row = next(datareader)
-        
-        prev_val = int(first_row[1])
+        if num_mo == 0:
+            dif_profit = profit
+            num_mo += 1
+            prev_val = profit
+        elif num_mo >= 0:
 
-        #calculate no. months
-        num_mo = len(list(datareader))+2
+            #calculate no. months
+            num_mo += 1
 
-        #avg delta...
-        avg = tot_profit / num_mo
+            #current difference to previous
+            dif_profit = profit - prev_val
+            
+            #max increase/decrease
+            if dif_profit > max_diff:
+                max_diff = dif_profit
+            elif dif_profit < min_diff:
+                min_diff = dif_profit
+            
+            dif_tot += dif_profit
+            
+            prev_val = profit
 
-        #current difference to previous
-        dif_profit = profit - prev_val
-        
-        #max increase/decrease
-        if dif_profit > max_diff:
-            max_diff = dif_profit
-        elif dif_profit < min_diff:
-            min_diff = dif_profit
+            
 
+#avg change...
+avg = dif_tot / num_mo
 
 #print above and produce to .txt #to_csv
-
 print("Financial Analysis.....")
 print(f"Total Months: {num_mo}")
 print(f"Total Profit: ${tot_profit}")
-print(f"Average Profit: ${avg}")
+print(f"Average Change: ${avg}")
 print(f"Greatest Increase: ${max_diff}")
 print(f"Greatest Decrease: ${min_diff}")
 
